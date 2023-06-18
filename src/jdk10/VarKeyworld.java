2ddc48de3656b8b800adf51633c57dedc92617cf
ZhangYawen <wes214@163.com>
2023-06-18 12:05:03 +0300

    Java10 var key world

---
+++ src/jdk10/VarKeyworld.java
@@ -0,0 +1,11 @@
+package jdk10;
+
+import java.util.List;
+
+public class VarKeyworld {
+    public static void main(String[] args) {
+        String text = "Hello Java 10";
+        var textVar = "Hello Java 10";
+        var list = List.of("A","B","C");
+    }
+}
