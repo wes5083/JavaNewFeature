9cde381689b298d9195ab29e6db319d0766876af
ZhangYawen <wes214@163.com>
2023-06-18 11:37:09 +0300

    Java11 synchronous Get

---
+++ src/jdk11/HttpClientDemo.java
@@ -0,0 +1,57 @@
+package jdk11;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+public class HttpClientDemo {
+
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("--------------------------synchronousGet-----------------------");
+            synchronousGet();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+
+
+    static void synchronousGet() throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://httpbin.org/get"))
+                .setHeader("User-Agent", "Java 11 HttpClient Bot") // add request header
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print response headers
+        HttpHeaders headers = response.headers();
+        headers.map().forEach((k, v) -> System.out.println(k + ":" + v));
+
+        // print status code
+        System.out.println(response.statusCode());
+
+        // print response body
+        System.out.println(response.body());
+    }
+
+}
