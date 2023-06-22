0d95fbcfcf8cc5c92e1f37b9197ddd43c3475775
ZhangYawen <wes214@163.com>
2023-06-22 23:49:23 +0300

    code format

--- src/jdk10/VarKeyworld.java
+++ src/jdk10/VarKeyworld.java
@@ -6,7 +6,7 @@ public class VarKeyworld {
     public static void main(String[] args) {
         String text = "Hello Java 10";
         var textVar = "Hello Java 10";
-        var list = List.of("A","B","C");
+        var list = List.of("A", "B", "C");
         System.out.println(list);
     }
 }
